008999fa239a00db3a1e59011a2cbeee0e6d8d4a
frank <fsimple47@gmail.com>
2023-08-21 17:18:03 +0300

    pushing to github

---
+++ first-name_c3/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+ 
+  return (
+    <div className="App">
+      <h1>Hello world</h1>
+      <p>I am Mugerwa Frank, this is my first React application.</p>
+    </div>
+  );
+}
+
+
+
+
+
+export default App;
